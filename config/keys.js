c36190b6902147ccf17115f54e9d2f7f3d566dc0
Adeola Adebayo <adeola.salvador@gmail.com>
2019-02-10 02:43:48 +0100

    Added create/delete comment routes - minor mods

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://adeola:adeola1@ds251894.mlab.com:51894/dev_devconnector",
+  mongoLocal: "mongodb://localhost:27017/dev_connector",
   secret: "Adeola"
 };
